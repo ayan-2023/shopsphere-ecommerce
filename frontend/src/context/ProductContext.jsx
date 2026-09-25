a49eaa5b503819a192d69079e7f4a2b5ecf23c32
Ubuntu <devopsadmin@test-vm.t15sexznbueedlnoqfy43qmxhh.rx.internal.cloudapp.net>
2026-09-25 07:29:32 +0000

    ProductContext.jsx

--- frontend/src/context/ProductContext.jsx
+++ frontend/src/context/ProductContext.jsx
@@ -8,7 +8,7 @@ import React, {
 
 const ProductContext = createContext();
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://74.225.168.175:5000/api';
 
 export const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
